7ddb1f6691f84342301fcb58d0fab2f13db47f7b
Eduardo <edwardviana@gmail.com>
2018-09-05 22:03:57 -0300

    first commit

---
+++ src/frontend/src/components/template/Header.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import './Logo.css'
+
+export default props =>
+    <header className="header">
+        Cabeçalhos
+    </header>
---
+++ src/frontend/src/main/Main.jsx
@@ -0,0 +1,11 @@
+import React,{Fragment} from 'react'
+import './Main.css'
+import Header from './Header'
+
+export default props =>
+    <React.Fragment>
+    <Header/>
+    <main className="Content">
+        Conteúdo
+    </main>
+    </React.Fragment>
